f0cc0550a84ab3290e1503eaf0af996a9b4f5a5e
Maazkhan999 <maazyousuf138@gmail.com>
2023-09-26 01:23:22 -0700

    header and hero-section and carseol setting done

---
+++ Assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    //slider js//
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        smartspeed:1000,
+        navText :['PREV','NEXT'],
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+               
+            }
+        }
+    })
+
+});
